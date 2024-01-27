fbd6643f1681153870e388a3aba70023b3b414b5
salman-setiawan <salmansetiawan88@gmail.com>
2024-01-28 04:52:50 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import IntroPages from "./pages/intro"
 
 export default function App() {
   return (
-    <div className="bg-[#111111] h-screen flex justify-center items-center px-16 text-white">
-      <IntroPages />
-    </div>
+    <IntroPages />
   )
 }
