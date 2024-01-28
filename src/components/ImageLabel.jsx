f2ed44341cebcd1ab61f6c1c4cff344e40984665
salman-setiawan <salmansetiawan88@gmail.com>
2024-01-29 00:18:18 +0800

    .

--- src/components/ImageLabel.jsx
+++ src/components/ImageLabel.jsx
@@ -17,14 +17,14 @@ const ImageLabel = ({title, desc1, desc2, chipdesc}) => {
         </div>
       </div>
       <div className='px-4 hidden md:block'>
-        <div className="flex flex-col gap-y-1">
-          <div className="flex items-center justify-center">
+        <div className="flex flex-col gap-y-1 justify-center items-center">
+          <div className="flex">
             <p className="text-[14px] font-semibold">{title}</p>
             {chipdesc &&
               <ChipShowcase text={chipdesc} bg='bg-[#30FFFC]' />
             }
           </div>
-          <div className='text-center'>
+          <div className='flex flex-col w-fit items-center'>
             <p className='text-[#9F9F9F] text-[12px]'>{desc1}</p>
             <p className='text-[#9F9F9F] text-[12px]'>{desc2}</p>
           </div>
