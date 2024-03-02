291421939ac8afa4711767a7cbc0ad0e43ee3998
salman-setiawan <salmansetiawan88@gmail.com>
2024-03-03 05:40:01 +0800

    improve popup on mobile

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -27,10 +27,10 @@ const Popup = () => {
 
   return (
     <div className={`fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center ${isOpen ? '' : 'hidden'}`}>
-      <div className="fixed bottom-10 right-4 bg-[#2b2b45] p-4 rounded-lg md:w-[520px] flex flex-col items-end">
-        <p className="text-[16px] mb-4">Selamat datang di halaman showcase, disini kamu dapat melihat koleksi dari hasil rancangan saya serta proses dibalik rancangan tersebut.</p>
-        <p className="text-[16px] mb-8">Kamu dapat berinteraksi dan melihat-lihat koleksi rancangan yang sudah dibuat dengan menggeser gambar ke kanan atau ke kiri menggunakan mouse pada PC atau hand-gesture pada smartphone/tablet.</p>
-        <button onClick={handleClose} className="py-2.5 px-3 bg-[#5454FF] text-white rounded-md text-[16px] font-semibold w-fit">Saya Mengerti</button>
+      <div className="fixed bottom-10 right-4 bg-[#2b2b45] p-4 rounded-lg w-[320px] md:w-[520px] flex flex-col items-end">
+        <p className="text-[12px] md:text-[16px] mb-2 md:mb-4">Selamat datang di halaman showcase, disini kamu dapat melihat koleksi dari hasil rancangan saya serta proses dibalik rancangan tersebut.</p>
+        <p className="text-[12px] md:text-[16px] mb-4 md:mb-8">Kamu dapat berinteraksi dan melihat-lihat koleksi rancangan yang sudah dibuat dengan menggeser gambar ke kanan atau ke kiri menggunakan mouse pada PC atau hand-gesture pada smartphone/tablet.</p>
+        <button onClick={handleClose} className="py-2.5 px-3 bg-[#5454FF] text-white rounded-md text-[12px] md:text-[16px] font-semibold w-fit">Saya Mengerti</button>
       </div>
     </div>
   );
