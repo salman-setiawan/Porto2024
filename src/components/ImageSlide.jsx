1a19ff2c78cf657d6fbfc9d2bb14ee41585bf94c
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-13 07:25:08 +0800

    testing bug

--- src/components/ImageSlide.jsx
+++ src/components/ImageSlide.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ImageSlide = ({ img }) => {
   return (
     <div className="flex justify-center px-4 pt-24 pb-40 md:pb-52 h-screen">
-    <img src={img} alt="showcase-img" className='object-contain' />
-  </div>
+      <img src={img} alt="showcase-img" className='object-contain' />
+    </div>
   )
 }
 
--- src/components/SlideNumber.jsx
+++ src/components/SlideNumber.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SlideNumber = ({current, totalCurrent}) => {
   return (
-    <div className="fixed top-0 pt-[64px] md:w-screen right-0 pr-6">
+    <div>
       <div className="w-full justify-center items-center flex space-x-2">
         <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
         <p className='text-white text-[12px] font-semibold'>{current}</p>
