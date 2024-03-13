e1341812dd06cfe748d825831127cd04fdabdb85
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-13 19:06:28 +0800

    fixing bug pt 2

--- src/components/ImageLabel.jsx
+++ src/components/ImageLabel.jsx
@@ -3,8 +3,8 @@ import ChipShowcase from './ChipShowcase'
 
 const ImageLabel = ({title, desc1, desc2, chipdesc}) => {
   return (
-    <div className="px-6 md:hidden">
-      <div className='fixed bottom-0 pb-[100px]'>
+    <div className="fixed bottom-0 pb-[100px] w-full px-6">
+      <div className='md:hidden'>
         <div className="flex justify-between items-center">
           <div>
             <p className="text-[14px] font-semibold">{title}</p>
@@ -16,20 +16,18 @@ const ImageLabel = ({title, desc1, desc2, chipdesc}) => {
           }
         </div>
       </div>
-      <div className="hidden md:block w-fit">
-        <div className="fixed bottom-0 pb-[100px] w-full">
-          <div className="flex justify-center">
-            <div className="flex flex-col gap-y-1 justify-center items-center">
-              <div className="flex">
-                <p className="text-[16px] pt-[2px] font-semibold">{title}</p>
-                {chipdesc &&
-                  <ChipShowcase text={chipdesc} bg='bg-[#30FFFC]' />
-                }
-              </div>
-              <div className='flex flex-col w-fit items-center'>
-                <p className='text-[#B5B5B5] text-[14px]'>{desc1}</p>
-                <p className='text-[#B5B5B5] text-[14px]'>{desc2}</p>
-              </div>
+      <div className="hidden md:block">
+        <div className="flex justify-center">
+          <div className="flex flex-col gap-y-1 justify-center items-center">
+            <div className="flex">
+              <p className="text-[16px] pt-[2px] font-semibold">{title}</p>
+              {chipdesc &&
+                <ChipShowcase text={chipdesc} bg='bg-[#30FFFC]' />
+              }
+            </div>
+            <div className='flex flex-col w-fit items-center'>
+              <p className='text-[#B5B5B5] text-[14px]'>{desc1}</p>
+              <p className='text-[#B5B5B5] text-[14px]'>{desc2}</p>
             </div>
           </div>
         </div>
