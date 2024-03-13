bacc45a84aad049a6a48e9665a291d5d11c68460
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-13 19:18:16 +0800

    fixing bug pt 3

--- src/components/SlideNumber.jsx
+++ src/components/SlideNumber.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 
 const SlideNumber = ({current, totalCurrent}) => {
   return (
-    <div className="fixed top-0 right-0 px-6 md:px-0 pt-[64px]">
-      <div className="md:w-screen justify-center items-center flex space-x-2">
-        <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
-        <p className='text-white text-[12px] font-semibold'>{current}</p>
-        <p className='text-[#B5B5B5] text-[12px]'>of</p>
-        <p className='text-white text-[12px] font-semibold'>{totalCurrent}</p>
+    <div className='fixed top-0 pt-[64px]'>
+      <div className="md:hidden">
+        <div className="w-screen justify-end items-center flex space-x-2 px-6">
+          <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
+          <p className='text-white text-[12px] font-semibold'>{current}</p>
+          <p className='text-[#B5B5B5] text-[12px]'>of</p>
+          <p className='text-white text-[12px] font-semibold'>{totalCurrent}</p>
+        </div>
+      </div>
+      <div className="hidden md:block">
+        <div className="w-screen justify-center items-center flex space-x-2">
+          <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
+          <p className='text-white text-[12px] font-semibold'>{current}</p>
+          <p className='text-[#B5B5B5] text-[12px]'>of</p>
+          <p className='text-white text-[12px] font-semibold'>{totalCurrent}</p>
+        </div>
       </div>
     </div>
   )
