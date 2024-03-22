42aa2176536e8ab7cdac76e0f8ca616ef610bc2b
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-22 12:02:32 +0800

    add tutorial on showcase

--- src/components/SlideNumber.jsx
+++ src/components/SlideNumber.jsx
@@ -3,16 +3,12 @@ import React from 'react'
 const SlideNumber = ({current, totalCurrent}) => {
   return (
     <div className='fixed top-0 pt-[64px]'>
-      <div className="md:hidden">
-        <div className="w-screen justify-end items-center flex space-x-2 px-6">
-          <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
-          <p className='text-white text-[12px] font-semibold'>{current}</p>
-          <p className='text-[#B5B5B5] text-[12px]'>of</p>
-          <p className='text-white text-[12px] font-semibold'>{totalCurrent}</p>
+      <div className="w-screen flex justify-between px-6">
+        <div className="flex flex-col">
+          <p className='text-[12px]'>Drag left or right to change slide.</p>
+          <p className='text-[12px]'>Click or tap to see content.</p>
         </div>
-      </div>
-      <div className="hidden md:block">
-        <div className="w-screen justify-center items-center flex space-x-2">
+        <div className="flex space-x-2">
           <p className='text-[#B5B5B5] text-[12px]'>Slide</p>
           <p className='text-white text-[12px] font-semibold'>{current}</p>
           <p className='text-[#B5B5B5] text-[12px]'>of</p>
