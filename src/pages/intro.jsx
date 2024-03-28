a7fbd85496695ca478a0f2441ad6f99512cde72a
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-28 21:07:17 +0800

    add loading state on intro

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,30 +1,50 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const IntroPages = () => {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
   useEffect(() => {
     const handlePageClick = () => {
-      window.location.href = "/collection";
+      if (!loading) {
+        window.location.href = "/collection";
+      }
     };
 
     document.addEventListener('click', handlePageClick);
 
+    // Hitung kemajuan loading
+    const interval = setInterval(() => {
+      if (progress < 100) {
+        setProgress(prevProgress => prevProgress + 1);
+      } else {
+        setLoading(false);
+      }
+    }, 50);
+
     return () => {
       document.removeEventListener('click', handlePageClick);
+      clearInterval(interval);
     };
-  });
+  }, [loading, progress]);
 
   return (
-    <div className="bg-[#111111] min-h-screen flex flex-col justify-center items-center px-8 text-white">
-      <div className='text-[16px] space-y-16 md:w-[640px]'>
-        <div className="space-y-8">
-          <img src="/assets/mark.png" alt="" className="h-[100px]" />
-          <div className="space-y-1">
-              <p>Hello there, my name is Salman.</p>
-              <p>I have experience in designing user experience, user interfaces, interaction design, design systems, and much more.</p>
-              <p>I hope you enjoy your time exploring the portfolio I have created.</p>
-          </div>
+    <div className="bg-[#171616] min-h-screen flex flex-col justify-center items-center px-8 text-white">
+      {loading ? (
+        <div className="flex flex-col justify-center items-center space-y-8 md:w-[640px]">
+          <img src="/icon.svg" alt="" className="h-[80px] w-fit" />
+          <p>Loading {progress}%</p>
+        </div>
+      ) : (
+        <div className='flex flex-col text-[16px] space-y-8 justify-center items-center md:w-[640px]'>
+          <img src="/icon.svg" alt="" className="h-[80px] w-fit" />
           <p className='blinking-text'>Click anywhere to continue.</p>
-          </div>
+        </div>
+      )}
+      <div className="fixed bottom-0 pb-10 text-center text-[13px] px-6">
+        <p>Hello there, my name is Salman.</p>
+        <p>I have experience in designing user experience, user interfaces, interaction design, design systems, and much more.</p>
+        <p>I hope you enjoy your time exploring the portfolio I have created.</p>
       </div>
     </div>
   );
