7806f74f240204abf5757ee937d4e3f190864daa
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-28 13:23:36 +0800

    improve showcase layout

--- src/components/ImageSlide.jsx
+++ src/components/ImageSlide.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ImageSlide = ({ img }) => {
   return (
-    <div className="flex justify-center px-4 pt-32 pb-52 h-screen">
+    <div className="flex justify-center px-4 pt-36 pb-48 md:pt-32 md:pb-52 h-screen">
       <img src={img} alt="showcase-img" className='object-contain' />
     </div>
   )
--- src/components/SlideNumber.jsx
+++ src/components/SlideNumber.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 
 const SlideNumber = ({current, totalCurrent, subject}) => {
   return (
-    <div className='fixed top-0 pt-[72px]'>
-      <div className="w-screen flex justify-between px-6">
+    <div className='fixed top-0 pt-[64px] md:pt-[72px]'>
+      <div className="w-screen flex flex-col-reverse md:flex-row md:justify-between px-6">
         <div className="flex flex-col">
           <p className='text-[12px]'>Drag left or right the image to change slide.</p>
           <p className='text-[12px]'>Click or tap the image to see more details.</p>
         </div>
-        <div className="flex flex-col items-end">
+        <div className="flex flex-col md:items-end">
           <div className="flex space-x-2 text-[12px]">
             <p className='text-[#B5B5B5]'>Slide</p>
             <p className='text-white font-semibold'>{current}</p>
             <p className='text-[#B5B5B5]'>of</p>
             <p className='text-white font-semibold'>{totalCurrent}</p>
           </div>
-          <p className="text-[12px] text-white font-semibold">{subject}</p>
+          <p className="text-[12px] text-white font-semibold hidden md:block">{subject}</p>
         </div>
       </div>
     </div>
