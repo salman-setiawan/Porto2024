a553ee19ad609d4fdb8c3e5690aa2bd7b7832045
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-31 05:33:59 +0800

    animate showcase

--- src/components/ImageSlide.jsx
+++ src/components/ImageSlide.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ImageSlide = ({ img }) => {
   return (
     <div className="flex justify-center px-4 pt-36 pb-48 md:pt-32 md:pb-52 h-screen">
-      <img src={img} alt="showcase-img" className='object-contain' />
+      <img src={img} alt="showcase-img" className='object-contain floating-element' />
     </div>
   )
 }
