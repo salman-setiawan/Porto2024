048170390a765970dfb7012a6b6b4627c40b29e3
Salman Setiawan <salmansetiawan88@gmail.com>
2024-03-30 13:04:06 +0800

    launch dataAssets feature

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,88 +1,67 @@
 import React, { useState, useEffect } from 'react';
+import dataAssets from '../data/assets';
 
 const IntroPages = () => {
   const [isAssetsLoading, setIsAssetsLoading] = useState(true);
-  const [loadingProgress, setLoadingProgress] = useState(0);
   const [progress, setProgress] = useState(0);
+  const [currentAsset, setCurrentAsset] = useState(null);
 
   useEffect(() => {
     const loadImage = (src) => {
       return new Promise((resolve, reject) => {
         const image = new Image();
-        image.onload = () => resolve();
+        image.onload = () => {
+          console.log('dataImage from:\n', src, '\nSuccessfully Loaded'); // Tampilkan pesan log saat gambar dimuat
+          resolve();
+        };
         image.onerror = (err) => reject(err);
         image.src = src;
       });
     };
 
-    const assetsToLoad = [
-      '/assets/showcase/showcase1.webp',
-      '/assets/showcase/showcase2.webp',
-      '/assets/showcase/showcase3.webp',
-      '/assets/showcase/showcase4.webp',
-      '/assets/showcase/showcase5.webp',
-      '/assets/showcase/showcase6.webp',
+    const loadVideo = (src) => {
+      return new Promise((resolve, reject) => {
+        const video = document.createElement('video');
+        video.oncanplaythrough = () => {
+          console.log('dataVideo from:\n', src, '\nSuccessfully Loaded'); // Tampilkan pesan log saat video dimuat
+          resolve();
+        };
+        video.onerror = (err) => reject(err);
+        video.src = src;
+        video.load();
+      });
+    };
 
-      '/assets/eduwork/article/teams.png',
-      '/assets/eduwork/article/flow.png',
-      '/assets/eduwork/article/process1.png',
-      '/assets/eduwork/article/process2.png',
-      '/assets/eduwork/gallery/screen1.webp',
-      '/assets/eduwork/gallery/screen2.webp',
-      '/assets/eduwork/gallery/screen3.webp',
-      '/assets/eduwork/gallery/screen4.webp',
-      '/assets/eduwork/gallery/screen5.webp',
-      '/assets/eduwork/gallery/screen6.webp',
-      '/assets/eduwork/gallery/screen7.webp',
-      '/assets/eduwork/gallery/screen8.webp',
-      '/assets/eduwork/gallery/screen9.webp',
-      '/assets/eduwork/gallery/screen10.webp',
-      '/assets/eduwork/gallery/screen11.webp',
-
-      '/assets/edufams/article/teams.png',
-      '/assets/edufams/article/flow.png',
-      '/assets/edufams/article/process1.png',
-      '/assets/edufams/article/process2.png',
-      '/assets/edufams/gallery/screen1.webp',
-      '/assets/edufams/gallery/screen2.webp',
-      '/assets/edufams/gallery/screen3.webp',
-      '/assets/edufams/gallery/screen4.webp',
-      '/assets/edufams/gallery/screen5.webp',
-      '/assets/edufams/gallery/screen6.webp',
-      '/assets/edufams/gallery/screen7.webp',
-      '/assets/edufams/gallery/screen8.webp',
-      '/assets/edufams/gallery/screen9.webp',
-      '/assets/edufams/gallery/screen10.webp',
-      '/assets/edufams/gallery/screen11.webp',
-
-      '/assets/avatar.png',
-      '/assets/document.svg',
-      '/assets/bgshowcase/showcase2.png',
-      '/assets/bio/sea.mp4',
-    ];
-
-    const totalAssets = assetsToLoad.length;
+    const totalAssets = dataAssets.length;
     const totalProgressSteps = 100;
     const progressStep = totalProgressSteps / totalAssets;
 
     let loadedAssets = 0;
-    let currentProgress = 0;
 
     const updateProgress = () => {
       loadedAssets++;
-      currentProgress += progressStep;
-      setProgress(Math.floor(currentProgress));
+      const newProgress = Math.floor((loadedAssets / totalAssets) * 100);
+      setProgress(newProgress);
 
-      if (loadedAssets === totalAssets) {
+      if (newProgress === totalProgressSteps) {
         setIsAssetsLoading(false);
       }
     };
 
-    
     const loadAssets = async () => {
-      for (const asset of assetsToLoad) {
-        await loadImage(asset);
-        updateProgress();
+      for (const asset of dataAssets) {
+        try {
+          setCurrentAsset(asset);
+          if (asset.endsWith('.webm')) {
+            await loadVideo(asset);
+          } else {
+            await loadImage(asset);
+          }
+          updateProgress();
+        } catch (error) {
+          console.error(`Error loading asset: ${asset}`, error);
+          // Handle error loading asset
+        }
       }
     };
 
@@ -93,24 +72,15 @@ const IntroPages = () => {
     };
   }, []);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (progress < 100) {
-        setProgress(prevProgress => prevProgress + 1);
-      }
-    }, 50);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, [progress]);
-
   return (
     <div className="bg-[#171616] min-h-screen flex flex-col justify-center items-center px-8 text-white">
       {isAssetsLoading ? (
         <div className="flex flex-col justify-center space-y-6 md:space-y-8 items-center md:w-[640px]">
           <img src="/icon.svg" alt="" className="h-[64px] md:h-[80px] w-fit" />
-          <p className='text-[14px] md:text-[16px]'>Loading {loadingProgress}%</p>
+          <div className="flex flex-col justify-center items-center space-y-1">
+          <p className='text-[14px] md:text-[16px]'>Loading {progress}%</p>
+          {currentAsset && <p className='text-[10px] md:text-[12px]'>loadProgress:{currentAsset}</p>}
+          </div>
         </div>
       ) : (
         <div className='flex flex-col text-[16px] space-y-6 md:space-y-8 justify-center items-center md:w-[640px]' onClick={() => window.location.href = "/collection"}>
